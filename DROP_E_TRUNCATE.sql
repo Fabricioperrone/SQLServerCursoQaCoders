e0545360432e0f20b9607a61dad77455ebd801d3
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-10-03 16:22:02 -0300

    DROP E TRUNCATE

---
+++ DROP_E_TRUNCATE.sql
@@ -0,0 +1,11 @@
+CREATE TABLE TabelaTeste(
+	TesteID int,
+	Nome varchar(255),
+	SobreNome varchar(255)
+);
+
+--Vamos deletar a tabela
+DROP TABLE TabelaTeste
+
+--Para deletar apenas os regsitros da tabela
+TRUNCATE TABLE TabelaTeste
